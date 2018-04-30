015ff653dc5d2345bc97c85294baece5459c5a33
Волошинский Максим <russhianhere@gmail.com>
2018-04-30 16:40:14 +0300

    404 page rework

--- Public/Errors/404.php
+++ Public/Errors/404.php
@@ -1,1 +1,13 @@
-geges
+<?php header("HTTP/1.0 404 Not Found"); ?>
+
+<html>
+
+  <head>
+    <title>Страница не найдена</title>
+  </head>
+
+  <body style="background: #3f5d81 url(https://vk.com/images/error404.png) no-repeat 50% 50%; width: 100%; height: 100%; overflow: hidden; margin: 0px;">
+    <a style="position: absolute; left: 50%; top: 50%; margin: -265px -345px 0px; height: 530px; width: 690px;" href="https://vk.com/"></a>
+  </body>
+
+</html>
